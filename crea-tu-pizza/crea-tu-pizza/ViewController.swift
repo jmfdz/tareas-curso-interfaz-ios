01b12ebe64c4d745f38f41691250797bfec0431f
jmfdz <josema.fernandez@ua.es>
2016-09-15 21:52:46 +0200

    Primera pantalla y primera tabla

---
+++ crea-tu-pizza/crea-tu-pizza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crea-tu-pizza
+//
+//  Created by Jose Maria Fernandez on 15/9/16.
+//  Copyright © 2016 Jose Maria Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
